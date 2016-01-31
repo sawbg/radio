ddab8b946d04e692d8ecdf835954653833334ad8
Andrew Wisner <awisner94@gmail.com>
2016-01-31 16:39:15 +0000

    Attempted adding some extra "header" bytes to the beginning of the stdout stream.

--- src/piped_test.cpp
+++ src/piped_test.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <cstdio>
+#include <unistd.h>
 
 #include "modulation.hpp"
 
 using namespace std;
+using namespace lolz;
 
 int main() {
-	const uint16 len = 256;
-	uint8 data[len];
-	DsbLcModulator mod(data, len);
+	const uint16 len = 48000;
+	float32 data[len];
+	uint16 index = 0;
+	float32 vals[48000];
+
+	for(int i = 0; i < 48000; i++) {
+		vals[i] = cos(2*M_PI*20000*(float)i/48000);
+	}
+
+	for(int i = 0; i <= 3; i++) {
+		cerr << "header" << endl;
+		putchar((int)255);
+	}
 
 	while(true) {
+		cerr << "loop" << endl;
+	
 		for(int i = 0; i < len; i++) {
-			data[i] = (uint8)getchar();
+			//data[i] = (uint8)getchar();
+			if(index >= 48000) index = 0;
+//			read(STDIN_FILENO, &data, len);
 		}
 
-		mod.Mod();
+//		mod.Mod();
 
 		for(int i = 0; i < len; i++) {
-			putchar((int)data[i]);
+			write(STDOUT_FILENO, &vals, len);
 		}
 
 	}
