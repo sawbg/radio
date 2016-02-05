b0f21944ff041f46f33f7f80690a7a776ff02ebb
Andrew Wisner <awisner94@gmail.com>
2016-02-05 13:27:23 +0000

    Began updating this class, but decided to use FFT/IFFT/Hilbert transform in function form.

--- src/ZDomain.hpp
+++ src/ZDomain.hpp
@@ -2,6 +2,7 @@
 #define ZDomain_H
 
 #include <cmath>
+#include <cstring>
 #include <complex>
 #include <functional>
 
@@ -17,12 +18,7 @@ namespace lolz {
 			/**
 			 *
 			 */
-			uint16 size;		
-
-			/**
-			 *
-			 */
-			float32* dest;
+			uint16 size;
 
 			/**
 			 *
@@ -42,6 +38,11 @@ namespace lolz {
 			/**
 			 *
 			 */
+			~ZDomain();
+
+			/**
+			 *
+			 */
 			void fft();
 
 			/**
@@ -53,17 +54,35 @@ namespace lolz {
 			 *
 			 */
 			void ifft();
+
+		protected:
+			/**
+			 *
+			 */
+			void _fft();
+
+			/**
+			 *
+			 */
+			void _ifft();
 	};
 
-	ZDomain::ZDomain(float32* orig, float32* dest, uint16 size) {
+	ZDomain::ZDomain(float32* orig, uint16 size) {
 		this->orig = orig;
-		this->dest = dest;
 		this->size = size;
 		temp = (std::complex<float32>*)malloc(size
 				* sizeof(std::complex<float32>*));
+		
+		for(uint32 i = 0; i < size; i++) {
+			temp[i] = std::complex<float32>(orig[i], 0);
+		}
 	}
 
-	void ZDomain::fft() {
+	ZDomain::~ZDomain() {
+		free(temp);
+	}
+
+	void ZDomain::_fft() {
 		// DFT
 		uint32 k = size;
 		uint32 n;
@@ -80,9 +99,9 @@ namespace lolz {
 			for(uint32 l = 0; l < k; l++) {
 				for(uint32 a = l; a < size; a += n) {
 					uint32 b = a + k;
-					std::complex<float32> t = x[a] - x[b];
-					x[a] += x[b];
-					x[b] = t * T;
+					std::complex<float32> t = temp[a] - temp[b];
+					temp[a] += temp[b];
+					temp[b] = t * T;
 				}
 
 				T *= phiT;
@@ -90,7 +109,7 @@ namespace lolz {
 		}
 
 		// Decimate
-		uint8 m = (uint32)log2(size);
+		uint32 m = (uint32)log2(size);
 
 		for(uint16 a = 0; a < size; a++) {
 			uint16 b = a;
@@ -99,25 +118,33 @@ namespace lolz {
 			b = (((b & 0xaaaa) >> 1) | ((b & 0x5555) << 1));
 			b = (((b & 0xcccc) >> 2) | ((b & 0x3333) << 2));
 			b = (((b & 0xf0f0) >> 4) | ((b & 0x0f0f) << 4));
-	//		b = (((b & 0xff00ff00) >> 8) | ((b & 0x00ff00ff) << 8));
+			b = (((b & 0xff00) >> 8) | ((b & 0x00ff) << 8));
 			b = ((b >> 8) | (b << 8)) >> (16 - m);
 
 			if (b > a)
 			{
-				std::complex<float32> t = x[a];
-				x[a] = x[b];
-				x[b] = t;
+				std::complex<float32> t = temp[a];
+				temp[a] = temp[b];
+				temp[b] = t;
 			}
 		}
 	}
+
+	void ZDomain::_ifft() {
+
+	}
+
+	/*void ZDomain::fft(std::complex<float32> dest) {
+		
+	}*/
 
 	void ZDomain::hilbert() {
 
 	}
 
-	void ZDomain::ifft {
+	/*void ZDomain::ifft {
 
-	}
+	}*/
 
 }
 
