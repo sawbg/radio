e0e731c1b4aede10603bbd4a7be4962faeed6604
Andrew Wisner <awisner94@gmail.com>
2016-02-15 10:35:35 +0000

    Got baseband filter working!

--- src/Filter.hpp
+++ src/Filter.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file
+ * @author Samuel Andrew Wisner, awisner94@gmail.com
+ * @brief Defines the Filter class.
+ */
+
 #ifndef Filter_H
 #define Filter_H
 
@@ -6,64 +12,99 @@
 
 #include "definitions.hpp"
 
-namespace lolz {
+namespace radio {
 	/**
+	 * This class implements a z-domain filter on a specified array of float32'''s
+	 * (a.k.a. singles, floats). It requires the transfer function coefficients
+	 * already be calculated (i.e., it does not generate the coefficients based
+	 * on desired filter characteristics). MATLAB and its Signal Processing
+	 * Toolbox can be used to generate the coefficients.
 	 *
+	 * While this class is designed to implement a single-section filter,
+	 * several instances of the class can be created and run over the data array
+	 * sequentially to effectively implement a multi-section filter.
+	 *
+	 * The class is designed (but not tested!) to allow for a z-domain transfer
+	 * function with different orders of the zeros (numerator) and poles
+	 * (denominator).
 	 */
 	class Filter {
 		public:
 			/**
+			 * Initializes Filter based on a difference equation. 
 			 *
+			 * @param data array to be filtered. The filtered data will be
+			 * placed here.
+			 *
+			 * @param size number of elements in the data array
+			 *
+			 * @param diffEq a vector containing two vectors of float32'''s
+			 * (a.k.a. singles, floats), containing the numerator and
+			 * denominator coefficients, respectively, of the z-domain tranfer
+			 * function of the filter in decending order (z^0, z^-1, z^-2,
+			 * etc.).
 			 */
-			Filter(float32* data, uint32 size,
-					std::vector<std::vector<float32>>& diffEq);
+			Filter(float32* data, uint32 size,fparams& diffEq);
 
 			/**
-			 *
+			 * Passes the data array through the digital filter and accounts for
+			 * x[n] and y[n] values from the previous call to Pass().
 			 */
 			void Pass();
 
 		protected:
 			/**
-			 *
+			 * The order of the filter transfer function (i.e., the maximum of
+			 * the orders of the numerator and denominator).
 			 */
 			uint8 eqLength;
 
 			/**
-			 *
+			 * The number of elements in the data array.
 			 */
 			uint32 size;
 
 			/**
-			 *
+			 * A pointer to the data array that should be filtered when Pass()
+			 * is called.
 			 */
 			float32* data;
 
 			/**
-			 *
+			 * A vector containing two vectors of float32'''s (a.k.a. singles,
+			 * floats), containing the numerator and denominator coefficients,
+			 * respectively, of the z-domain tranfer function of the filter
+			 * in decending order (z^0, z^-1, z^-2, etc.).
 			 */
-			std::vector<std::vector<float32>> diffEq;
+			fparams diffEq;
 
 			/**
-			 *
+			 * Vectors of the original (x[n]) and filtered (y[n]) values of the
+			 * data array used to calculate the first filtered values of the
+			 * data array. In spite of the type name, this variable does NOT
+			 * contains filter parameters but rather the same data type that
+			 * fparams represents.
 			 */
-			std::vector<std::vector<float32>> prev;
+			fparams prev;
 	};
 
-	Filter::Filter(float32* data, uint32 size,
-			std::vector<std::vector<float32>>& diffEq) {
+	Filter::Filter(float32* data, uint32 size, fparams& diffEq) {
 		this->data = data;
 		this->size = size;
 		this->diffEq = diffEq;
 		eqLength = std::max(this->diffEq[NUM].size(),
 				this->diffEq[DEN].size());
 
+		for(int i = 0; i < 2; i++) {
+			prev.push_back(std::vector<float32>());
+		}
+
 		for(int i = 0; i < this->diffEq[NUM].size(); i++) {
-			prev[NUM].push_back(0);
+			prev[OLD].push_back(0);
 		}
 
 		for(int i = 0; i < this->diffEq[DEN].size(); i++) {
-			prev[DEN].push_back(0);
+			prev[NEW].push_back(0);
 		}
 	}
 
@@ -76,24 +117,24 @@ namespace lolz {
 
 			for(int j = 0; j < prev[OLD].size(); j++) {
 				temp[i] += diffEq[NUM][j] * (j > i 
-						? prev[OLD][prev[OLD].size() - (j - i)] : data[j - i]);
+						? prev[OLD][prev[OLD].size() - (j - i)] : data[i - j]);
 			}
 
-			for(int j = 0; j < prev[NEW].size(); j++) {
+			for(int j = 1; j < prev[NEW].size(); j++) {
 				temp[i] -= diffEq[DEN][j] * (j > i
-						? prev[NEW][prev[NEW].size() - (j - i)] : temp[j - i]);
+						? prev[NEW][prev[NEW].size() - (j - i)] : temp[i - j]);
 			}
 		}
 
 		// create the REST of the values in filtered data
-		for(int i = 0; i < size; i++) {
+		for(int i = eqLength; i < size; i++) {
 			temp[i] = 0;
 
 			for(int j = 0; j < diffEq[NUM].size(); j++) {
 				temp[i] += diffEq[NUM][j] * data[i - j];
 			}
 
-			for(int j = 0; j < diffEq[NUM].size(); j++) {
+			for(int j = 1; j < diffEq[NUM].size(); j++) {
 				temp[i] -= diffEq[DEN][j] * temp[i - j];
 			}
 		}
--- src/baseband_filter_test.cpp
+++ src/baseband_filter_test.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file
+ * @author Samuel Andrew Wisner, awisner94@gmail.com
+ * @brief Tests sinusoidal tone generation
+ */
+
 #include <iostream>
 #include <cmath>
 #include <cstdio>
@@ -9,22 +15,31 @@
 #include "zdomain.hpp"
 
 using namespace std;
-using namespace lolz;
+using namespace radio;
 
+/**
+ * This prgram tests and demonstrates the Filter class and the baseband
+ * low-pass filter (fp = 1.7 kHz, fs = 3 kHz, Ap = 0.5 dB, As = 60 dB).
+ */
 int main() {
-	const uint16 len = 16384;
+	const uint32 len = 65536;
 	float32 data[len];
-	Filter baseFilter(data, len, F_BASEBAND);
+	float32 temp[len];
+	Filter baseFilter(temp, len, F_BASEBAND);
 
 	for(int i = 0; i < len; i++) {
-		data[i] = 0.25 * (sin(2*M_PI*440*i/48000)
+		data[i] = 0.95 * (sin(2*M_PI*440*i/48000)
 				+ sin(2*M_PI*2000*i/48000) + sin(2*M_PI*2500*i/48000)
 				+ sin(2*M_PI*3000*i/48000));
 	}
 
 	while(true) {
+		for(int i = 0; i < len; i++) {
+			temp[i] = data[i];
+		}
+
 		baseFilter.Pass();
-		write(STDOUT_FILENO, &data,  len * sizeof(float32));
+		write(STDOUT_FILENO, &temp,  len * sizeof(float32));
 	}
 
 }
