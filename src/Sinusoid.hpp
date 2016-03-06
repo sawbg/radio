545648065484cf5a29a6f71842d43669eccf5592
Andrew Wisner <awisner94@gmail.com>
2016-03-05 21:57:50 -0600

    Added documentation.

--- src/Sinusoid.hpp
+++ src/Sinusoid.hpp
@@ -12,65 +12,72 @@
 #include "definitions.hpp"
 
 namespace radio {
+/**
+ * This class creates an easy-to-call sinusoid that will preserve its phase
+ * throughout its lifespan. Essentially, it is a ring buffer.
+ */
 	class Sinusoid {
 		public:
 			/**
-			 *
+			 * Creates a ring-buffer sinusoid.
 			 */
-			Sinusoid(float32 carrierFreq, uint32 samplingRate);
+			Sinusoid(float32 frequency, uint32 samplingRate);
 
 			/**
-			 *
+			 * Free arrays malloc'd in the constructor.
 			 */
 			~Sinusoid();
 
 			/**
-			 *
+			 * Provides the next value of the sinusoid in a manner consistant
+			 * with a ring buffer.
 			 */
 			float32 next();
 
 			/**
-			 *
+			 * Provides the next value of the sinusoid shifted 90 degrees in a
+			 * manner consistant with a ring buffer.
 			 */
 			float32 nextShifted();
 
 		private:
 			/**
-			 *
+			 * The frequency of the sinusoid
 			 */
-			float32 carrierFreq;
+			float32 frequency;
 
 			/**
-			 *
+			 * The current index of the sinusoid's arrays
 			 */
 			uint32 carrierIndex = 0;
 
 			/**
-			 *
+			 * The sampling rate
 			 */
 			uint32 samplingRate;
 
 			/**
-			 *
+			 * Initialized as an array of the sinusoid values
 			 */
 			float32* sinusoid;
 
 			/**
-			 *
+			 * Initialized as an array of the sinusoid values shifted 90
+			 * degrees
 			 */
 			float32* sinusoidShift90;
 	};
 
-	Sinusoid::Sinusoid(uint32 carrierFreq, uint32 samplingRate) {
-		this->carrierFreq = carrierFreq;
+	Sinusoid::Sinusoid(uint32 frequency, uint32 samplingRate) {
+		this->frequency = carrierFreq;
 		this->samplingRate = samplingRate;
 		sinusoid = (float32*)std::malloc(samplingRate * sizeof(float32));
 		sinusoidShift90 = (float32*)std::malloc(samplingRate * sizeof(float32));
 
-		float32 arg = 2 * M_PI * carrierFreq / samplingRate;
+		float32 arg = 2 * M_PI * frequency / samplingRate;
 
 		for(uint32 i; i < samplingRate; i++) {
-			// cosine argument evaluates as float due to M_PI and carrierFreq
+			// cosine argument evaluates as float due to M_PI and frequency
 			sinusoid[i] = cos(arg * i);
 			sinusoidShift90[i] = sin(arg * i);
 		}
