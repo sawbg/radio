52b35ef320a7c34b2ece6aa324182e1bf78d50b1
Andrew Wisner <awisner94@gmail.com>
2016-03-10 18:38:05 -0600

    Updated to show Nick.

--- src/piped_test.cpp
+++ src/piped_test.cpp
@@ -13,23 +13,9 @@ int main() {
 	const uint16 len = 16384;
 	float32 data[len];
 	float32 iqData[2*len];
-	DsbScModulator mod(data, len);
-
-	vector<vector<float32>> baseFilterVector = { vector<float32> {
-
-
-	Filter baseFilter;
-
-
-	for(int i = 0; i < len; i++) {
-		//data[i] = sin(2*3.141592*440*i/len);
-	}
-
-
 
 	while(true) {
 		read(STDIN_FILENO, &data, len * sizeof(float32));
-		//mod.Mod();
 		makeIQ(data, iqData, len);
 		write(STDOUT_FILENO, &iqData,  2 * len * sizeof(float32));
 	}
