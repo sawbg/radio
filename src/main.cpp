ca3e3bf0d4af258f085278649dc32407787031ca
Andrew Wisner <awisner94@gmail.com>
2016-03-22 21:25:29 -0500

    Got code to the point it will compile. Haven't tested it yet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,63 @@
-#include <iostream>
+/**
+ * @author Samuel Andrew Wisner, awisner94@gmail.com
+ * @brief contains the "brains" of the entire project
+ */
+
 #include <cstdio>
+#include <iostream>
+#include <string>
 #include <unistd.h>
 
+#include "auxiliary.hpp"
 #include "Filter.hpp"
-#include "modulation.hpp"
+#include "Modulator.hpp"
 #include "zdomain.hpp"
 
 using namespace std;
 using namespace radio;
 
-int main() {
-	
-	
-	
-	
-	
-	
-	const uint16 len = 16384;
-	float32 data[len];
-	float32 iqData[2*len];
-	Filter baseFilter;
+int main(int argc, char* argv[]) {
+
+	// Constants
+	const uint8 ERROR = -1;
+	const uint8 NUM_TYPES = 8;
+	const uint16 BUFFER_SIZE = 16384;
+	const float32 FREQ_MIN = 0;
+	const float32 FREQ_MAX = 62;
+
+	// Ensure 1 or 2 arguments given
+	if(argc < 2) {
+		std::cerr << "Error: too many arguments!" << std::endl;
+		ShowHelp();
+		return ERROR;
+	} else if(argc > 3) {
+		std::cerr << "Error: too few arguments!" << std::endl;
+		ShowHelp();
+		return ERROR;
+	}
+
+	// Declare primative Variables
+	float32 dataBuffer[BUFFER_SIZE];
+	float32 iqBuffer[2 * BUFFER_SIZE];
+	ModulationType type;
+
+	try{
+		type = to_type(string(argv[1]));
+	} catch(std::exception ex) {
+		std::cerr << ex.what() << std::endl << std::endl;
+		ShowHelp();
+		return ERROR;
+	}
+
+	// Declare objects
+	Filter baseFilter(dataBuffer, BUFFER_SIZE, F_BASEBAND);
+	Modulator modulator(dataBuffer, BUFFER_SIZE, type);
 
 	while(true) {
-		read(STDIN_FILENO, &data, len * sizeof(float32));
-		//mod.Mod();
-		makeIQ(data, iqData, len);
-		write(STDOUT_FILENO, &iqData,  2 * len * sizeof(float32));
+		read(STDIN_FILENO, &dataBuffer, BUFFER_SIZE * sizeof(float32));
+		baseFilter.Pass();
+		modulator.Mod();
+		makeIQ(dataBuffer, iqBuffer, BUFFER_SIZE);
+		write(STDOUT_FILENO, &iqBuffer,  2 * BUFFER_SIZE * sizeof(float32));
 	}
 }
